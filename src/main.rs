884ee9671ea4f529895643219187fa88b134f4e5
Nikita Belov <wtedst@gmail.com>
2021-06-29 22:12:14 +0200

    Add projects

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde::{Serialize, Deserialize};
 use std::fs;
 use std::collections::BTreeMap;
 use rocket::response::content;
+use rocket::http::ContentType;
 use rocket::State;
 use handlebars::{Handlebars};
 
@@ -17,39 +18,31 @@ struct Strings {
 
 #[derive(Serialize, Deserialize)]
 struct Config {
+	rerender: bool,
 	sources: BTreeMap<String, String>,
 	templates: BTreeMap<String, String>
 }
 
 type PageMap = BTreeMap<String, String>;
 
-struct Pages<'a> {
-	strings: Strings,
-	hb: Handlebars<'a>,
-	map: PageMap
+struct Pages {
+	templates: PageMap,
 }
 
-impl Pages<'_> {
-	fn new() -> Self {
+impl Pages {
+	fn new(templates: PageMap) -> Self {
 		Pages { 
-			strings: toml::from_str(&get_string("resources/strings.toml")).unwrap(),
-			hb: Handlebars::new(),
-		 	map: PageMap::new() 
-		}
-	}
-
-	fn populate(&mut self, templs: &BTreeMap<String, String>) {
-		dbg!(&self.strings.arrays);
-		for (name, templ) in templs {
-			let hb_source = get_string(templ);
-			self.hb.register_template_string(name, hb_source);
-			let page = self.hb.render(name, &self.strings).unwrap();
-			self.map.insert(name.to_string(), page.clone());
+		 	templates
 		}
 	}
 
 	fn get(&self, name: &str) -> String {
-		self.map.get(name).unwrap().to_string()
+		let strings: Strings = toml::from_str(&get_string("resources/strings.toml")).unwrap();
+		let templ_path = self.templates.get(name).unwrap();
+		let templ_string = get_string(&templ_path);
+		let mut hb = Handlebars::new();
+		hb.register_template_string(name, templ_string);
+		hb.render(name, &strings).unwrap()
 	}
 }
 
@@ -58,10 +51,24 @@ fn get_string(path: &str) -> String {
 	return fs::read_to_string(path).unwrap();
 }
 
+fn get_file(name: &str) -> Option<fs::File> {
+	let path = std::path::Path::new("resources").join(&name);
+	return fs::File::open(path).ok();
+}
+
 #[get("/")]
-fn index(pages: &State<Pages>) -> content::Html<String> {
-	let s = pages.get("index");
+fn index(pages: &State<Pages>, config: &State<Config>) -> content::Html<String> {
+	let s: String = pages.get("index");
 	return content::Html(s);
+}
+
+#[get("/<filename>", format = "image/webp")]
+fn image_png(filename: &str) -> Option<(ContentType, fs::File)> {
+	if let Some(file) = get_file(filename) {
+		return Some((ContentType::PNG, file));
+	}
+	
+	Option::None
 }
 
 #[get("/styles", format = "css")]
@@ -74,10 +81,10 @@ fn rocket() -> _ {
 
 	let config: Config = toml::from_str(&get_string("resources/config.toml")).unwrap();
 
-	let mut pages = Pages::new();
-	pages.populate(&config.templates);
+	let mut pages = Pages::new(config.templates.clone());
 
     rocket::build()
-    	.mount("/", routes![index, styles])
+    	.mount("/", routes![index, styles, image_png])
     	.manage(pages)
+    	.manage(config)
 }
