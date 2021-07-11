1b7e2e89b25b80486503755a59b147899c082d2d
Nikita Belov <wtedst@gmail.com>
2021-07-12 00:16:27 +0200

    Start moving to Actix

---
+++ src/config.rs
@@ -0,0 +1,96 @@
+
+//use serde::{Serialize, Deserialize};
+use std::fs;
+use std::fs::{File};
+use std::net::{Ipv4Addr, SocketAddrV4};
+use std::io::BufReader;
+use std::str::FromStr;
+
+use toml;
+use toml::Value;
+
+
+use rustls::internal::pemfile::{certs, pkcs8_private_keys};
+use rustls::{NoClientAuth, ServerConfig};
+
+
+enum Field {
+	Host,
+	Port
+}
+
+impl Field {
+	fn key(&self) -> &'static str {
+		return match self {
+			Field::Host => "host",
+			Field::Port => "port"
+		};
+	}
+}
+
+//#[derive(Serialize, Deserialize)]
+pub struct Config {
+	pub socket: SocketAddrV4,
+
+	key: Option<String>,
+	certs: Option<String>
+}
+
+pub struct TlsConfig<'c> {
+	key: &'c str,
+	certs: &'c str
+}
+
+type TomlMap = toml::map::Map<String, toml::Value>;
+
+fn socket(map: &TomlMap) -> Option<SocketAddrV4> {
+	let hostval = map.get(Field::Host.key())?;
+	let portval = map.get(Field::Port.key())?;
+	if let (Value::String(string), Value::Integer(int)) = (hostval, portval) {
+		let host = Ipv4Addr::from_str(&string).ok()?;
+		return Some(SocketAddrV4::new(host, *int as u16));
+	}
+
+	None
+}
+
+impl Config {
+
+	pub fn load() -> Option<Self> {
+		let bytes = fs::read("Config.toml").ok()?;
+		let map: TomlMap = toml::from_slice(&bytes).ok()?;
+
+		if let Some(socket) = socket(&map) {
+			return Some(Config { socket, key: None, certs: None });
+		}
+		
+		None
+	}
+
+	pub fn tls<'c>(&'c self) -> Option<TlsConfig<'c>> {
+		if let (Some(key), Some(certs)) = (&self.key, &self.certs)  {
+			let config = TlsConfig {
+				key: key.as_ref(),
+				certs: certs.as_ref()
+			};
+
+			return Some(config);
+		}  else {
+			return None;
+		}
+	}
+
+	pub fn rustls_config(&self) -> Option<ServerConfig> {
+
+		let tls_files = self.tls()?;
+		let mut config = ServerConfig::new(NoClientAuth::new());
+    	let cert_file = &mut BufReader::new(File::open(tls_files.certs).ok()?);
+    	let key_file = &mut BufReader::new(File::open(tls_files.key).ok()?);
+    	let cert_chain = certs(cert_file).ok()?;
+    	let mut keys = pkcs8_private_keys(key_file).ok()?;
+    	// keys.remove(0) to take an owned value
+    	config.set_single_cert(cert_chain, keys.remove(0)).ok()?;
+    	return Some(config);
+	}
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,24 @@
-#[macro_use] extern crate rocket;
 
-use toml;
-use serde::{Serialize, Deserialize};
-use std::fs;
-use std::collections::BTreeMap;
-use rocket::response::content;
-use rocket::http::ContentType;
-use rocket::State;
-use handlebars::{Handlebars};
+mod config;
 
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use config::Config;
 
-#[derive(Serialize, Deserialize)]
-struct Strings {
-	strings: BTreeMap<String, String>,
-	arrays: BTreeMap<String, Vec<String>>
-}
+// #[get("/")]
+// async fn index() -> impl Responder {
 
-#[derive(Serialize, Deserialize)]
-struct Config {
-	rerender: bool,
-	sources: BTreeMap<String, String>,
-	templates: BTreeMap<String, String>
-}
+// }
 
-type PageMap = BTreeMap<String, String>;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
 
-struct Pages {
-	templates: PageMap,
-}
-
-impl Pages {
-	fn new(templates: PageMap) -> Self {
-		Pages { 
-		 	templates
-		}
-	}
-
-	fn get(&self, name: &str) -> String {
-		let strings: Strings = toml::from_str(&get_string("resources/strings.toml")).unwrap();
-		let templ_path = self.templates.get(name).unwrap();
-		let templ_string = get_string(&templ_path);
-		let mut hb = Handlebars::new();
-		hb.register_template_string(name, templ_string);
-		hb.render(name, &strings).unwrap()
-	}
-}
+	let config = Config::load();
 
 
-fn get_string(path: &str) -> String {
-	return fs::read_to_string(path).unwrap();
-}
-
-fn get_file(name: &str) -> Option<fs::File> {
-	let path = std::path::Path::new("resources").join(&name);
-	return fs::File::open(path).ok();
-}
-
-#[get("/")]
-fn index(pages: &State<Pages>, config: &State<Config>) -> content::Html<String> {
-	let s: String = pages.get("index");
-	return content::Html(s);
-}
-
-#[get("/<filename>", format = "image/webp")]
-fn image_png(filename: &str) -> Option<(ContentType, fs::File)> {
-	if let Some(file) = get_file(filename) {
-		return Some((ContentType::PNG, file));
-	}
-	
-	Option::None
-}
-
-#[get("/styles", format = "css")]
-fn styles() -> content::Css<String> {
-	return content::Css(get_string("resources/styles.css"));
-}
-
-#[launch]
-fn rocket() -> _ {
-
-	let config: Config = toml::from_str(&get_string("resources/config.toml")).unwrap();
-
-	let mut pages = Pages::new(config.templates.clone());
-
-    rocket::build()
-    	.mount("/", routes![index, styles, image_png])
-    	.manage(pages)
-    	.manage(config)
+    HttpServer::new(|| {
+        App::new().route("/", web::get().to(|| HttpResponse::Ok()))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
